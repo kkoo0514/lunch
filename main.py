eb720065a87bc2946917587edb2a0554c4ea87de
KHJ <1993@coreit.co.kr>
2025-05-09 16:30:11 +0900

    백엔드

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector
+from mysql.connector import errorcode
+import os
+
 
 app = FastAPI(title="FastAPI Backend")
 
@@ -14,7 +18,42 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to FastAPI Backend"}
+    result = []
+    cnx = None
+    cursor = None
+    message = 'success'
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+        )
+        # 2) 커넥션 생성
+        cursor = conn.cursor()
+
+        # 3) 쿼리 실행
+        cursor.execute("SELECT * FROM t_place;")
+        rows = cursor.fetchall()
+
+        result = rows
+
+    except mysql.connector.Error as err:
+        # 에러 처리
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            message = "사용자 이름 또는 비밀번호 오류"
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            message = "데이터베이스가 없습니다"
+        else:
+            message = err
+    finally:
+        # 4) 자원 정리
+        if cursor:
+            cursor.close()
+        if cnx:
+            cnx.close()
+    return {"message": message,"data":result}
 
 @app.get("/health")
 async def health_check():
