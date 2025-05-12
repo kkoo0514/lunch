0fbd84a0b7ddc7afdca1cd337a4c5a799fda7084
서하늘 <0617@coreit.co.kr>
2025-05-12 15:04:38 +0900

    front 화면 적용

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import MainRoutes from './MainRoutes';
+import AuthRoutes from './AuthRoutes';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+export const router = createRouter({
+    // history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/:pathMatch(.*)*',
+            component: () => import('@/views/pages/Error404.vue')
+        },
+        MainRoutes,
+        AuthRoutes
+    ]
+});
 
-export default router
